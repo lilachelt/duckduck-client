c97f43bd98ac097588d52a7d5aefbdc1e80f4f09
lilachelt <lilach.elt@gmail.com>
2023-06-02 14:38:02 +0300

    add grid to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Container, Typography } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import Input from './components/Input';
 import Results from './components/Results';
 import Sidebar from './components/SideBar';
 
 const App: React.FC = () => {
   return (
-    <Container maxWidth="md">
-      <Typography variant="h4" component="h1" align="center" mt={4} mb={2}>
-        DuckDuckGo Search
-      </Typography>
-      <Input />
-      <Results />
-      <Sidebar/>
+<Container>
+      <h1>DuckDuckGo Search</h1>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Input />
+            <Results />
+          </Grid>
+          <Grid item xs={6}>
+            <Sidebar />
+          </Grid>
+        </Grid>
     </Container>
   );
 };
