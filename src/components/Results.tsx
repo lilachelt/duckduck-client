ea1273746ab156d0e85d9e6f6a5a21d24635155b
lilachelt <lilach.elt@gmail.com>
2023-06-02 14:36:48 +0300

    update results logic

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,23 +1,62 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
-import { Typography } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 import Pagination from './Pagination';
 import '../css/Results.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Results: React.FC = () => {
-  const results = useSelector((state: RootState) => state.reducer.results);
+  const { results, query } = useSelector((state: RootState) => state.reducer);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+
+  useEffect(() => {
+    setCurrentPage(1)
+  },[query]);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  let paginatedData = results.slice(startIndex, endIndex);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+
+  function markQuery(title : string) {
+    const regex = new RegExp(`(${query})`, 'gi');
+    const parts = title.split(regex);
+  
+    return (
+      <span>
+        {parts.map((part, index) =>
+          regex.test(part) ? <span key={index} className="marked">{part}</span> : part
+        )}
+      </span>
+    );
+  }
+  const shouldDisplayError = () => !results.length && query !== '';
 
   return (
-    <div>
-      {results.map((result: any, index: any) => (
-        <div key={index}>
-          <a href={result.url}>
-            <Typography variant="h6">{result.title}</Typography>
-          </a>
+    <Box mt={2}>
+      {paginatedData.map((item : any) => (
+        <div className='item'>
+          <Link href={item.url} target="_blank" rel="noopener">
+          {markQuery(item.title)}
+          </Link>
         </div>
       ))}
-    </div>
+      <div>
+     {shouldDisplayError() && <Typography> No results founds</Typography>}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={Math.ceil(results.length / itemsPerPage)}
+        onPageChange={handlePageChange}
+      />
+    </Box>
+    
   );
 };
 
